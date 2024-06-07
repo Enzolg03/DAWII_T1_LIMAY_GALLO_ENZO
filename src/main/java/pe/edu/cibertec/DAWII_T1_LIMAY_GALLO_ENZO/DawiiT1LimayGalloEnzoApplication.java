7fe4c5c7ffc593a0c227a48969f3102c99d8a059
Enzolg03 <enzojoaquinlimaygallo@gmail.com>
2024-06-07 16:30:35 -0500

    primer commit

---
+++ src/main/java/pe/edu/cibertec/DAWII_T1_LIMAY_GALLO_ENZO/DawiiT1LimayGalloEnzoApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWII_T1_LIMAY_GALLO_ENZO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiiT1LimayGalloEnzoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiiT1LimayGalloEnzoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DAWII_T1_LIMAY_GALLO_ENZO/DawiiT1LimayGalloEnzoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWII_T1_LIMAY_GALLO_ENZO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiiT1LimayGalloEnzoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
